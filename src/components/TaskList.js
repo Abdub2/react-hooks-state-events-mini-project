b2b91997ef5d53bc82b844a2cf7f03cbaa12313e
Abdub2 <abdubdoyo2005@gmail.com>
2023-01-23 12:24:26 +0300

    Complete assignment

--- src/components/TaskList.js
+++ src/components/TaskList.js
@@ -1,11 +1,19 @@
 import React from "react";
+import Task from "./Task";
 
-function TaskList() {
-  return (
-    <div className="tasks">
-      {/* display a list of tasks using Task component */}
-    </div>
-  );
+function TaskList({ tasks, deleteTasks }) {
+	return (
+		<div className="tasks">
+			{tasks.map((list) => (
+				<Task
+					key={list.text}
+					text={list.text}
+					deleteTasks={deleteTasks}
+					category={list.category}
+				/>
+			))}
+		</div>
+	);
 }
 
 export default TaskList;
